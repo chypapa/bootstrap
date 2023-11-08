606edaae9ab7b6c3063163ece0f3b8056ecf542c
chypapa <120268301+chypapa@users.noreply.github.com>
2023-11-08 03:01:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+console.log('Hello!');
+
+// const tooltips = document.querySelectorAll('.tt')
+// tooltips.forEach(t => {
+//     new bootstrap.Tooltips(t)
+// })
+
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+    const modal = document.querySelectorAll('.modal fade')
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            
+            form.classList.add('was-validated')
+        }, false)
+        })
+    })()
+
+    
